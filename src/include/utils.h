a7ab863613bcedd93acf107a803a1aebacfc2630
Patrick Pataky <patrick.pataky@epfl.ch>
2025-12-03 18:44:53 +0100

    Implemented simple unit tests for tinyLFU and the bloom filters

--- src/include/utils.h
+++ src/include/utils.h
@@ -8,7 +8,7 @@
 // W = sample size (in bytes) = 2^SAMPLE_SIZE_BITS
 // Must be much larger than CACHE_SIZE_BITS
 // Number of requests that are tracked before the reset().
-#define SAMPLE_SIZE_BITS (10 * CACHE_SIZE_BITS)
+#define SAMPLE_SIZE_BITS (CACHE_SIZE_BITS + 4)
 
 // Number of bits used for each counter in the counting bloom filter
 // In the paper, each counter can count up to W / C.
--- src/src/counting_bloom.c
+++ src/src/counting_bloom.c
@@ -53,7 +53,7 @@ static inline void set_counter(struct counting_bloom *cb, size_t idx,
 
 bool counting_bloom_add(struct counting_bloom *cb, uint64_t addr)
 {
-    if (!cb) return;
+    if (!cb) return false;
 
     size_t   min_counter_idx   = 0;
     uint64_t min_counter_value = UINT64_MAX;
@@ -61,6 +61,7 @@ bool counting_bloom_add(struct counting_bloom *cb, uint64_t addr)
     struct hashes hs;
     get_hashes(addr, &hs);
 
+    // 1. Find min value
     for (size_t i = 0; i < NUM_HASH_FUNCTIONS; i++) {
         uint32_t hash = hs.h[i];
         size_t idx = hash % cb->size;
@@ -68,13 +69,26 @@ bool counting_bloom_add(struct counting_bloom *cb, uint64_t addr)
         uint64_t val = get_counter(cb, idx);
         if (val < min_counter_value) {
             min_counter_value = val;
-            min_counter_idx = idx;
         }
     }
 
-    set_counter(cb, min_counter_idx, min_counter_value + 1);
+    // 2. Update all counters that are minimal
+    uint64_t new_min = min_counter_value + 1;
+    if (new_min > COUNTER_MASK) {
+        new_min = COUNTER_MASK;
+    }
 
-    return (min_counter_value + 1) >= (1ULL << SAMPLE_SIZE_BITS);
+    for (size_t i = 0; i < NUM_HASH_FUNCTIONS; i++) {
+        uint32_t hash = hs.h[i];
+        size_t idx = hash % cb->size;
+        
+        uint64_t val = get_counter(cb, idx);
+        if (val <= new_min) {
+            set_counter(cb, idx, new_min);
+        }
+    }
+
+    return new_min >= COUNTER_MASK;
 }
 
 uint64_t counting_bloom_estimate(struct counting_bloom *cb, uint64_t addr)
